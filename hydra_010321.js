efabff66435e5494f3475876c0592ea7d58e37fa
markdenardo <mark.denardo@gmail.com>
2021-01-04 20:19:02 -0500

    010321

--- hydra_010321.js
+++ hydra_010321.js
@@ -1,2 +1,14 @@
-osc(1,1,1).rotate(1,100).repeat(1).modulate(o0).out(o0)
+
+//shows fft (sound)
+a.show()
+//sets fft bins
+a.setBins(6)
+
+s1.initScreen()
+src(s1).modulate(o1).out(o1)
+
+
+// osc(1,1,()=>a.fft[1]).rotate(1,100).repeat(1).modulate(o0).modulate(o1).out(o0)
+osc(1,1,1).rotate(1,100).repeat(1,1).modulate(o1).out(o0)
+
 render(o0)
