6bef7dac48c60fc199072e0e327728e5f72feba4
markdenardo <mark.denardo@gmail.com>
2021-01-07 23:54:50 -0500

    010721

--- hydra_010721.js
+++ hydra_010721.js
@@ -1,9 +1,8 @@
-
 a.show()
 a.setBins(6)
 
 s1.initScreen()
-src(s1).modulate(o0).modulate(o1).out(o0)
+src(s1).modulate(o0).modulate(o1).saturate( ()=>a.fft[3]*10 ).out(o0)
 
 osc(1,1,1).pixelate(1,1,1).modulate(o0).out(o1)
 render(o0)
