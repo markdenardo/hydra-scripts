b1fb7b9fccb0be22ee21d1d06a57f0ae9c666cf3
markdenardo <mark.denardo@gmail.com>
2021-01-14 19:31:17 -0500

    011421_1

--- hydra_011421.js
+++ hydra_011421.js
@@ -1,10 +1,21 @@
+// a.show()
+// a.setBins(6)
+
+// s1.initScreen()
+
+// src(s1).modulate(o0).modulate(o1).out(o0)
+// noise( 10, ()=>a.fft[4] *4).repeatX( 100, 10 ).modulate(o0).modulate(o1).modulate(o3).out(o1)
+// shape(3).pixelate(2,()=>a.fft[1]).repeat().out(o2)
+
+// render(o2)
+
 a.show()
 a.setBins(6)
 
 s1.initScreen()
 
-src(s1).modulate(o0).modulate(o1).out(o0)
+src(s1).modulate(o0).modulate(o1).modulate(o2).out(o0)
 noise( 10, ()=>a.fft[4] *4).repeatX( 100, 10 ).modulate(o0).modulate(o1).modulate(o3).out(o1)
-shape(3).pixelate(2,()=>a.fft[1]).repeat().out(o2)
+shape(3).pixelate(2,10).repeat().out(o2)
 
-render(o2)
+render(o0)
