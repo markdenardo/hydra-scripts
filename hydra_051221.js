3aede9965e42508ce2b1efe1372b7970932e3cf5
markdenardo <mark.denardo@gmail.com>
2021-05-12 18:52:46 -0400

    iteration

--- hydra_051221.js
+++ hydra_051221.js
@@ -11,3 +11,21 @@ src(o1).modulate(o1).out(o2);
 src(o2).modulate(o2).rotate(1, 1).out(o3);
 
 render(o2);
+
+
+a.show();
+
+s0.initScreen();
+
+src(s0).out(o0);
+
+shape(() => a.fft[1] * 2)
+  .modulate(o0)
+  .repeat(9, () => a.fft[1] * 2)
+  .out(o1);
+
+src(o1).modulate(o1).out(o2);
+
+src(o2).modulate(o2).modulate(o3).rotate(1, 2).out(o3);
+
+render(o1);
